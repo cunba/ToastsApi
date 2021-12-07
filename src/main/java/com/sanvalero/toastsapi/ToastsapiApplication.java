63cf9ae76f50387ce00adb8c5b5e4ba702b94a44
cunba <ire.cunba@gmail.com>
2021-12-07 01:42:14 +0100

    main

---
+++ src/main/java/com/sanvalero/toastsapi/ToastsapiApplication.java
@@ -0,0 +1,13 @@
+package com.sanvalero.toastsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToastsapiApplication.class, args);
+	}
+
+}
