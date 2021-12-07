f866a19c7ab091f64a1fe2f488da5061d56e5c42
cunba <ire.cunba@gmail.com>
2021-12-07 02:00:30 +0100

    coffee model

---
+++ src/main/java/com/sanvalero/toastsapi/model/Coffee.java
@@ -0,0 +1,24 @@
+package com.sanvalero.toastsapi.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "Coffee")
+public class Coffee {
+
+    @Id
+    private int id;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "date")
+    private Date date;
+    @Column(name = "menu")
+    private boolean menu;
+    @Column(name = "price")
+    private float price;
+    @Column(name = "punctuation")
+    private float punctuation;
+}
