e00a8b88b63494344e4c2b3846f2d20c7a640931
cunba <ire.cunba@gmail.com>
2021-12-10 21:01:36 +0100

    creacion publicationRepository

---
+++ src/main/java/com/sanvalero/toastsapi/repository/PublicationRepository.java
@@ -0,0 +1,30 @@
+package com.sanvalero.toastsapi.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.sanvalero.toastsapi.model.Establishment;
+import com.sanvalero.toastsapi.model.Publication;
+import com.sanvalero.toastsapi.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublicationRepository extends CrudRepository<Publication, Integer> {
+    List<Publication> findByDate(LocalDate date);
+
+    List<Publication> findByDateBetween(LocalDate minDate, LocalDate maxDate);
+
+    List<Publication> findByTotalPrice(float price);
+
+    List<Publication> findByTotalPriceBetween(float minPrice, float maxPrice);
+
+    List<Publication> findByTotalPunctuation(float punctuation);
+
+    List<Publication> findByTotalPunctuationBetween(float minPunctuation, float maxPunctuation);
+
+    List<Publication> findByEstablishment(Establishment establishment);
+
+    List<Publication> findByUser(User user);
+}
