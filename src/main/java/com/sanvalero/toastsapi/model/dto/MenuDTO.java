41f93ffbd97ff07cb48dac7549f650118f44b943
cunba <ire.cunba@gmail.com>
2022-04-27 21:43:42 +0200

    añadir anotaciones en dtos

--- src/main/java/com/sanvalero/toastsapi/model/dto/MenuDTO.java
+++ src/main/java/com/sanvalero/toastsapi/model/dto/MenuDTO.java
@@ -1,5 +1,10 @@
 package com.sanvalero.toastsapi.model.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MenuDTO {
+    @NotNull
+    @Positive
     private float price;
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 5)
     private float punctuation;
 }
--- src/main/java/com/sanvalero/toastsapi/model/dto/UserDTO.java
+++ src/main/java/com/sanvalero/toastsapi/model/dto/UserDTO.java
@@ -1,5 +1,11 @@
 package com.sanvalero.toastsapi.model.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +14,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String username;
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String name;
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String surname;
+    @Past
     private String birth_date;
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Email
     private String email;
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String password;
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String role;
 }
